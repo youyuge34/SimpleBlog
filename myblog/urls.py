6b7bc34c5a45a054e31b2c2e01c1c377d9c4bfb4
Andy <1197993367@qq.com>
2017-09-05 20:26:47 +0800

    添加article功能完成

--- myblog/urls.py
+++ myblog/urls.py
@@ -14,8 +14,9 @@ from django.conf.urls import url, include
 import views
 
 urlpatterns = [
-    #用r'^$'约束空字符串
-    url(r'^index/$',views.blog),
-    url(r'article/(?P<article_id>[0-9]+)/$',views.article_page,name='article_page')
-
+    # 用r'^$'约束空字符串
+    url(r'^index/$', views.blog),
+    url(r'^article/(?P<article_id>[0-9]+)/$', views.article_page, name='article_page'),
+    url(r'^edit/$', views.edit_page, name='edit_page'),
+    url(r'^edit/action/$', views.edit_action, name='edit_action'),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -11,3 +11,20 @@ def blog(request):
 def article_page(request,article_id):
     article = models.Article.objects.get(pk = article_id)
     return render(request,'myblog/article_page.html',{'article':article})
+
+
+def edit_page(request):
+    return render(request, 'myblog/edit_page.html')
+
+
+def edit_action(request):
+    """
+    新增文章逻辑
+    :param request:
+    :return:
+    """
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    models.Article.objects.create(title=title, content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'myblog/index.html', {'articles': articles})
